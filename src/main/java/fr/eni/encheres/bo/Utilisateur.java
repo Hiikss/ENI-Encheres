3c2e23973e03e530c0277057fb551af02db6010a
Maud <marieaude.torchy2022@campus-eni.fr>
2023-03-01 16:36:03 +0100

    Ajout Attributs utilisateur

--- src/main/java/fr/eni/encheres/bo/Utilisateur.java
+++ src/main/java/fr/eni/encheres/bo/Utilisateur.java
@@ -1,6 +1,21 @@
 package fr.eni.encheres.bo;
 
 public class Utilisateur {
+	private int noUtilisateur;
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String telephone;
+	private String rue;
+	private String codePostal;
+	private String ville;
+	private String motDePasse;
+	private int credit;
 	
+	private ArticleVendu vend;
+	private Enchere encherit;
+	
+	private int administrateur;
 
 }
