6f273fb908b4d4e2558f659992858e63b9353c82
Thomas <91778476+Hiikss@users.noreply.github.com>
2023-03-02 19:23:49 +0100

    FIX - changement nom fonction

--- src/main/java/fr/eni/encheres/dal/DAOFactory.java
+++ src/main/java/fr/eni/encheres/dal/DAOFactory.java
@@ -3,7 +3,7 @@ package fr.eni.encheres.dal;
 
 public abstract class DAOFactory {
 	
-	public static UtilisateurDAO getAvisDAO()
+	public static UtilisateurDAO getUtilisateurDAO()
 	{
 		return new UtilisateurDAOJdbcImpl();
 	}
