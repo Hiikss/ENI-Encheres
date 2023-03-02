962958a387559e3eba9809fb53f15e90d658651b
Maud <marieaude.torchy2022@campus-eni.fr>
2023-03-02 09:36:44 +0100

    Ajout constructeur, Getter/Setter, toString pour Enchere

--- src/main/java/fr/eni/encheres/bo/Enchere.java
+++ src/main/java/fr/eni/encheres/bo/Enchere.java
@@ -6,5 +6,54 @@ public class Enchere {
 	private LocalDate dateEnchere;
 	private int montantEchere;
 	private ArticleVendu article;
+	
+	
+	
+	/**
+	 * 
+	 */
+	public Enchere() {
+		super();
+	}
+	/**
+	 * @param dateEnchere
+	 * @param montantEchere
+	 * @param article
+	 */
+	public Enchere(LocalDate dateEnchere, int montantEchere, ArticleVendu article) {
+		super();
+		this.dateEnchere = dateEnchere;
+		this.montantEchere = montantEchere;
+		this.article = article;
+	}
+	public LocalDate getDateEnchere() {
+		return dateEnchere;
+	}
+	public void setDateEnchere(LocalDate dateEnchere) {
+		this.dateEnchere = dateEnchere;
+	}
+	public int getMontantEchere() {
+		return montantEchere;
+	}
+	public void setMontantEchere(int montantEchere) {
+		this.montantEchere = montantEchere;
+	}
+	public ArticleVendu getArticle() {
+		return article;
+	}
+	public void setArticle(ArticleVendu article) {
+		this.article = article;
+	}
+	@Override
+	public String toString() {
+		return "Enchere [dateEnchere=" + dateEnchere + ", montantEchere=" + montantEchere + ", article=" + article
+				+ "]";
+	}
+	
+	
+	
+	
+	
+	
 
 }
