6d00e4551981fe76971c4528e086e5e1430788a6
Thomas <91778476+Hiikss@users.noreply.github.com>
2023-03-02 11:53:34 +0100

    Add - ajout class UtilisateurManager

---
+++ src/main/java/fr/eni/encheres/bll/UtilisateurManager.java
@@ -0,0 +1,28 @@
+package fr.eni.encheres.bll;
+
+import fr.eni.encheres.bo.Utilisateur;
+import fr.eni.encheres.dal.DAOFactory;
+import fr.eni.encheres.dal.UtilisateurDAO;
+
+public class UtilisateurManager {
+
+	private UtilisateurDAO utilisateurDAO;
+	
+	
+	public UtilisateurManager() {
+		utilisateurDAO = DAOFactory.getUtilisateurDAO();
+	}
+	
+	public Utilisateur seConnecter(String identifiant, String mdp) {
+		
+		Utilisateur utilisateur = new Utilisateur();
+		if(identifiant.contains('@'))
+			utilisateur.setEmail(identifiant);
+		else
+			utilisateur.setPseudo(identifiant);
+			
+		utilisateur.setMotDePasse(mdp);
+		
+		this.utilisateurDAO.seConnecter(utilisateur);
+	}
+}
