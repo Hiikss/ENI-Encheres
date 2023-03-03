f3aa7f24e6375699046753e85d23b18d09f26150
Maud <marieaude.torchy2022@campus-eni.fr>
2023-03-03 15:46:25 +0100

    Création class CodesResultatBLL.java

---
+++ src/main/java/fr/eni/encheres/bll/CodesResultatBLL.java
@@ -0,0 +1,11 @@
+package fr.eni.encheres.bll;
+
+public class CodesResultatBLL {
+	
+	public static final int REGLE_EMAIL_ERREUR=20000;
+	
+	public static final int REGLE_MOT_DE_PASSE_ERREUR=20001;
+	
+	public static final int REGLE_PSEUDO_ERREUR=20002;
+
+}
