6be776e4c28bac75f6dbcfb2c2ba1ae97273a242
Thomas <91778476+Hiikss@users.noreply.github.com>
2023-03-08 16:09:54 +0100

    Create EnchereManager.java

---
+++ src/main/java/fr/eni/encheres/bll/EnchereManager.java
@@ -0,0 +1,30 @@
+package fr.eni.encheres.bll;
+
+import java.util.List;
+
+import fr.eni.encheres.BusinessException;
+import fr.eni.encheres.bo.Enchere;
+import fr.eni.encheres.dal.DAOFactory;
+import fr.eni.encheres.dal.EnchereDAO;
+
+public class EnchereManager {
+	
+	private EnchereDAO enchererDAO;
+	private static EnchereManager instance;
+
+	public EnchereManager() {
+		enchererDAO = DAOFactory.getEnchereDAO();
+	}
+
+	public static EnchereManager getInstance() {
+		if (instance == null) {
+			instance = new EnchereManager();
+		}
+		return instance;
+	}
+
+	public List<Enchere> selectAll() throws BusinessException {
+		return this.enchererDAO.selectAll();
+	}
+
+}
