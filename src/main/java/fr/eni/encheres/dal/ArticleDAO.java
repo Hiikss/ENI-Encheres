c215c99630e5e7fcb7e96088f534745138f9e539
Maud <marieaude.torchy2022@campus-eni.fr>
2023-03-08 10:51:38 +0100

    Création interface ArticleDAO

---
+++ src/main/java/fr/eni/encheres/dal/ArticleDAO.java
@@ -0,0 +1,10 @@
+package fr.eni.encheres.dal;
+
+import fr.eni.encheres.BusinessException;
+import fr.eni.encheres.bo.ArticleVendu;
+
+public interface ArticleDAO {
+	
+	public void insert(ArticleVendu article) throws BusinessException;
+
+}
